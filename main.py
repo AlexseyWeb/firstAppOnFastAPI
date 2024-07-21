a1d697a2adb5ea0e87fea5e7578e29bb5f9ecffa
Алексей Гусаков <alongenet32@gmail.com>
2024-07-21 18:55:13 +0300

    add path /tasks

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
+
+class Task(BaseModel):
+    name: str
+    description: Optional[str] = None
+
+
 @app.get("/")
 async def home():
-    return "Starting project on FastAPI"
+    return {"data": "First Page!"}
+
+@app.get("/tasks")
+async def get_tasks():
+    task = Task(name="Изучить FastAPI")
+    return {"data": task}
