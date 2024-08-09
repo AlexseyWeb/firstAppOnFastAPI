5c24fed17d6124eacab82b4643d329bed682739b
Алексей Гусаков <alongenet32@gmail.com>
2024-08-09 09:10:47 +0300

    add route /txt

--- main.py
+++ main.py
@@ -51,24 +51,7 @@ async def get_who(
 
 @app.get("/login")
 async def login(creds: HTTPBasicCredentials = Depends(basic)) -> dict:
-    rules_admin = """Beautiful is better than ugly.
-                        Explicit is better than implicit.
-                        Simple is better than complex.
-                        Complex is better than complicated.
-                        Flat is better than nested.
-                        Sparse is better than dense.
-                        Readability counts.
-                        Special cases aren't special enough to break the rules.
-                        Although practicality beats purity.
-                        Errors should never pass silently.
-                        Unless explicitly silenced.
-                        In the face of ambiguity, refuse the temptation to guess.
-                        There should be one-- and preferably only one --obvious way to do it.
-                        Although that way may not be obvious at first unless you're Dutch.
-                        Now is better than never.
-                        Although never is often better than *right* now.
-                        If the implementation is hard to explain, it's a bad idea.
-                        If the implementation is easy to explain, it may be a good idea. """
+    rules_admin = "You administrator"
     if(creds.username == "Admin" and creds.password == "secret"):
         return {"data": "Hi Admin on API", "msg": rules_admin}
     raise HTTPException(status_code=401, detail="Sorry you're not admin on site!!!")
@@ -76,3 +59,7 @@ async def login(creds: HTTPBasicCredentials = Depends(basic)) -> dict:
 @app.get("/secret", response_class=HTMLResponse)
 async def get_secret_page(request: Request):
     return templates.TemplateResponse(request=request, name="secret.html")
+
+@app.get("/dzen")
+async def get_dzen():
+    return FileResponse("dzen.txt")
